4d17d123ca21e9fad5b2aea067b6abb6f01ec20e
Stormdrev <stormdrev@gmail.com>
2024-04-10 16:36:50 +0300

    Add project files.

---
+++ Source/DroneWar/KillAllTurretsGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KillAllTurretsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRONEWAR_API AKillAllTurretsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
